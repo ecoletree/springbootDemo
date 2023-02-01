b0a40b47008193b940f4b9df802c19e8db1dfb8d
ecoletree <55118813+ecoletree@users.noreply.github.com>
2023-02-01 10:42:15 +0900

    kh file

---
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/BootWorkout1Application.java
@@ -0,0 +1,13 @@
+package com.bootWorkout.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWorkout1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWorkout1Application.class, args);
+	}
+
+}
