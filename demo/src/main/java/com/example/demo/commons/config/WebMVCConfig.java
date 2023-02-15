12485a869003a5559619c43b2ba30e3c58be5a47
M.K. <mk@ecoletree.com>
2023-02-15 11:23:41 +0900

    pom에 아파치 commons 관련 추가 multipartResolver 추가 mybatis 관련 소스 추가

--- demo/src/main/java/com/example/demo/commons/config/WebMVCConfig.java
+++ demo/src/main/java/com/example/demo/commons/config/WebMVCConfig.java
@@ -2,7 +2,9 @@ package com.example.demo.commons.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -34,6 +36,14 @@ public class WebMVCConfig implements WebMvcConfigurer {
         .addResourceLocations("/webjars/")
         .resourceChain(false)
         ;
+    }
+    
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+        commonsMultipartResolver.setMaxUploadSize(50 * 1024 * 1024);
+        return commonsMultipartResolver;
     }
 
 }
