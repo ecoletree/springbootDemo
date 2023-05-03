f42e09b776abf02c8124b6da8c0a21cb29f4973a
kkh <rudgus327@ecoletree.com>
2023-05-03 19:37:57 +0900

    멀티 셀렉트 수정중 Signed-off-by: kkh <rudgus327@ecoletree.com>

--- bootWorkout-1/src/main/resources/static/service/home.js
+++ bootWorkout-1/src/main/resources/static/service/home.js
@@ -16,6 +16,8 @@
 	ctrl.formId = "#loginForm";
 	ctrl.JWT = null;
 	ctrl.groupList= null;
+	ctrl.optionList = {};
+	ctrl.selectedList = {};
 	// ============================== 화면 컨트롤 ==============================
 	/**
 	 * init VIEW
@@ -23,43 +25,95 @@
 	ctrl.init = function(initData) {
 		var self = et.vc;
 		self.groupList = initData.groupList;
+		
+//		self.setOption(["selGroup","selGroup2"]);
 		self.selectChangeHandler();
+		$("#btnEdit").click(self.btnEditHandler);
+		$("#btnClear,#btnClear2").click(self.btnClearHandler);
 		$("#selGroup,#selCenter").change(self.selectChangeHandler);
+		$("#selGroup2,#selCenter2").change(self.selectChangeHandler);
 		
 		
 	};
+	
+	
+	/**
+		1. 초기화
+	 */
+	ctrl.btnClearHandler = function(){
+		var self = et.vc;
+		var groupname = $(this).data("groupname");
+		var groupList = self.groupList;
+		self.selectedList[groupname] = [];
+		var options = self.setSValue(groupname);
+		self.makeTreeSelectOption(groupList,options);
+	}
+	/**
+		1. 수정 
+	 */
+	ctrl.btnEditHandler = function(){
+		var self = et.vc;
+		var groupList = self.groupList;
+		var sVal =[];
+		var groupname = "selGroup2";
+		$('input[data-groupname=iptGroup]').each(function(index,element){
+			sVal.push(element.value);
+		});
+		self.selectedList[groupname] = sVal;
+		var options = self.setSValue(groupname);
+		
+		self.makeTreeSelectOption(groupList,options);
+		
+	}
+	/**
+		리스트, 셀렉트 이름으로 옵션에 sValue 설정
+	 */
+	ctrl.setSValue = function(groupname){
+		var self = et.vc;
+		var list = self.selectedList[groupname];
+		var options = self.optionList[groupname];
+		Object.keys(options).forEach(function(v){
+			options[v].sValue = list[v]??"";
+		})
+		return options;
+	}
 	
 	ctrl.selectChangeHandler = function(){
 		var self = et.vc;
 		
 		var groupList = self.groupList;
+	
 		
 		var options = [];
 		var option1 = {value:"group_id", text:"view_group_name", targetID:"#selGroup",default:"그룹전체"};
 		var option2 =  {value:"tenant_id", text:"view_group_name", targetID:"#selCenter", default:"센터전체"};
-		var option3 = {value:"group_cd", text:"view_group_name", targetID:"#selTeam", default:"팀전체"};
+		var option3 = {value:"team_id", text:"view_group_name", targetID:"#selTeam", default:"팀전체"};
 		
 		options.push(option1,option2,option3);
 		
+		var options2 = [];
+		
+		var option4 = {value:"group_id", text:"view_group_name", targetID:"#selGroup2",default:"그룹전체"};
+		var option5 =  {value:"tenant_id", text:"view_group_name", targetID:"#selCenter2", default:"센터전체"};
+		var option6 = {value:"team_id", text:"view_group_name", targetID:"#selTeam2", default:"팀전체"};
+		
+		options2.push(option4,option5,option6);
+		
+		self.optionList["selGroup"] = options;
+		self.optionList["selGroup2"] = options2;
+		
 		self.makeTreeSelectOption(groupList,options);
+		self.makeTreeSelectOption(groupList,options2);
 	}
 	
 	// ============================== 동작 컨트롤 ==============================
-	/**
-	 * 트리 구조 셀렉트 박스 생성
-	 * 
-	 * @param {array} aDataList option으로 보여질 데이터 리스트
-	 * @param {array} oOptionList option들에 value와 text 값의 필드명을 각각의 오브젝트로 담은 리스트
-	 							, {value: "",text :"", targetID:"#targetID", default:""} 고정
-	 * @param {String} ftargetID option이 들어가게 될 최상위 select의 id 값, #selectID 형태
-	 */
 	ctrl.makeTreeSelectOption = function(aDataList,oOptionList){
-		
 		var targetData = aDataList;
 		var option;
 		for(var i in oOptionList){
 			var targetID = oOptionList[i].targetID;
-			var sValue = $(targetID).val(); // selected value
+			
+			var sValue = oOptionList[i]?.sValue ?? $(targetID).val();
 			$(targetID).empty();
 			
 			var optionValue = oOptionList[i].value; 
@@ -98,6 +152,7 @@
 			 * 선택된 value가 옵션 값에 있을 때 
 			 */
 			if (sValue) {
+				
 				if(optionValues.includes(sValue)){
 					$(targetID).val(sValue);
 				}
--- bootWorkout-1/src/main/webapp/WEB-INF/views/home.jsp
+++ bootWorkout-1/src/main/webapp/WEB-INF/views/home.jsp
@@ -11,12 +11,30 @@
 <script  src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head> 
 <body>
-<h1>Spring boot</h1>
+<br><br>
+<div style="margin-left : 20px ">
 <div>
-<h3>3</h3>
-<select id="selGroup" ></select>
-<select id="selCenter" ></select>
-<select id="selTeam" ></select>
+<h4>조회</h4>
+<select id="selGroup" data-groupname ="selGroup"></select>
+<select id="selCenter" data-groupname ="selGroup" ></select>
+<select id="selTeam" data-groupname ="selGroup" ></select>
+<button id="btnClear"data-groupname ="selGroup">select 초기화 (추가 팝업)</button>
+</div>
+<br>
+<div>
+<h4>수정</h4>
+<select id="selGroup2" data-groupname ="selGroup2"></select>
+<select id="selCenter2" data-groupname ="selGroup2" ></select>
+<select id="selTeam2" data-groupname ="selGroup2" ></select>
+<button id="btnClear2"data-groupname ="selGroup2">select 초기화 (추가 팝업)</button>
+</div>
+<div>
+<input id="iptGroup"  data-groupname ="iptGroup"/>
+<input id="iptCenter"  data-groupname ="iptGroup"/>
+<input id="iptTeam"  data-groupname ="iptGroup"/>
+<button id="btnEdit">select 수정</button>
+
+</div>
 </div>
 	
 <!-- <script src="//code.jquery.com/jquery-1.11.0.min.js"></script> -->
