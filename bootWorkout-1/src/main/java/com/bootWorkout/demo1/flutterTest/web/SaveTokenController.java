bbd8306f69f2dd1f18895c3c18ffce230c99f21c
kkh <rudgus327@ecoletree.com>
2023-08-28 13:29:25 +0900

    fcm수정

--- bootWorkout-1/src/main/java/com/bootWorkout/demo1/flutterTest/web/SaveTokenController.java
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/flutterTest/web/SaveTokenController.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins="*")
-@RequestMapping("/flutter")
+@RequestMapping("/restFlutter")
 @Slf4j
 public class SaveTokenController extends ETBaseController{
 	
