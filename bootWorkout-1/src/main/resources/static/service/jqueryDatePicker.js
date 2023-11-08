9d02a94b80b431514eacb8d8bba903b6b389dceb
khkim <ecoletree@gamil.com>
2023-11-08 10:17:29 +0900

    jquery date picker js 사용시 필요없는 부분 삭제

--- bootWorkout-1/src/main/resources/static/service/jqueryDatePicker.js
+++ bootWorkout-1/src/main/resources/static/service/jqueryDatePicker.js
@@ -24,145 +24,22 @@
 	ctrl.init = function(initData) {
 		var self = et.vc;
 		
-		
-//		4. jQuery date range picker
-		// default
-//		new DateRangePicker("#divJQueryRange0").setPicker();
-		// single
+		//single
 		new DateRangePicker("#divJQueryRange0",true).setInitDate('2023.05.25').setPicker(); // single
 		// range
 		new DateRangePicker("#divJQueryRange1").setMaxDate(7).setInitDate('2023.05.21','2023.05.28').setPicker();
 		new DateRangePicker("#divJQueryRange2").setMaxDate(7).setInitDate(new Date(),5).setPicker();
 		new DateRangePicker("#divJQueryRange3").setExtendsCalendar(true).setDisableSelectForward().setPicker();
 		new DateRangePicker("#divJQueryRange4").setExtendsCalendar(true).setDisableSelectBackward().setPicker();
-//		$("#divJQueryRange0").click(self.btnJqOpenHandler);
 	};
 	// ==================== 4. jQuery Date Range Picker ======================
 	
-	ctrl.defaultJQueryDateRangePicker = function(id){
-		var self = et.vc;
-		
-		var picker = {
-			autoClose: false,
-			format: 'YYYY-MM-DD',
-			separator: ' 부터 ', // 캘린더 상단에 라벨에 들어가는 구분자
-			language: 'ko', // ko로 설정해야함 
-			startOfWeek: 'sunday',// or monday
-			getValue: function() // --> 이거뭔데
-			{
-				debugger;
-//				return $(this).val();
-				if ($('#iptJQueryRange1').val() && $('#iptJQueryRange2').val() ){
-//					debugger;
-//					console.log($('#iptJQueryRange1').val() + ' to ' + $('#iptJQueryRange2').val());
-					return $('#iptJQueryRange1').val() + ' 부터 ' + $('#iptJQueryRange2').val();
-				}
-				else{
-					return '';
-				}
-			},
-			setValue: function(s,s1,s2) //--> 캘린더에서 날자 선택하면 셋팅되는거
-			{
-				// single_ date일 때 function이 있어야 함
-				console.log("S"+s); // 범위일때는 **부터 ** 이렇게 나옴
-				$('#iptJQueryRange1').val(s1);
-				$('#iptJQueryRange2').val(s2);
-//				if(!$(this).attr('readonly') && !$(this).is(':disabled') && s != $(this).val())
-//				{
-//					$(this).val(s);
-//				}
-			},
-			startDate: false,
-			endDate: false,
-			time: {
-				enabled: true // true/false 시간 선택 가능 <-- 슬라이드 바 말고 다른형태로 가능한지 *********************
-			},
-			minDays: 0, // 최소 선택 일자
-			maxDays: 0, // 최대 선택일자
-			showShortcuts: true, // true/false : true면 보여 
-			shortcuts: // shortCut 설정 가능 <-- custom 시에 null *********************
-			{
-				//'prev-days': [1,3,5,7],
-				//'next-days': [3,5,7],
-				//'prev' : ['week','month','year'],
-				//'next' : ['week','month','year']
-			},
-			customShortcuts : [
-//				//if return an array of two dates, it will select the date range between the two dates
-				{
-					name: 'this week',
-					dates : function()
-					{
-						var start = moment().day(0).toDate();
-						var end = moment().day(6).toDate();
-						return [start,end];
-					}
-				},
-				{
-					name: 'this month',
-					dates : function()
-					{
-						var start = moment().startOf("month").toDate();
-						var end = moment().endOf('month').toDate();
-						return [start,end];
-					}
-				},
-//				//if only return an array of one date, it will display the month which containing the date. and it will not select any date range
-//				{
-//					name: 'Oct 2014',
-//					dates : function()
-//					{
-//						//move calendars to show this date's month and next month
-//						var movetodate = moment('2014-10','YYYY-MM').toDate();
-//						return [movetodate];
-//					}
-//				}
-				
-			], // shortCut 설정 가능 *********************
- 			inline:false, // true 면 화면에 계속 표시
-			container:'body',
-			alwaysOpen:false,
-			singleDate:false , // true 면 하루 선택만 해도 setValue function 실행  *********************
-			lookBehind: false,
-			batchMode: false,
-			duration: 200,
-			stickyMonths: true, // true면 달을 넘길때 붙어있는 달이 페어로 넘어간다 (날짜 선택에 지장 없음)
-			dayDivAttrs: [],
-			dayTdAttrs: [],
-			applyBtnClass: '',
-			singleMonth: 'auto', // true 면 한달씩 나옴 
-			hoveringTooltip: function(days, startTime, hoveringTime)
-			{
-				return days > 1 ? days + ' ' + /* lang('days')*/ "일" : '';
-			}, // 마우스 hover일때 일 수 세줌 
-			showTopbar: true, // 상단에 선택된 날짜 나타냄 (ex.기간:2023-05-02 부터 2023-05-04 (3일간))
-			swapTime: false,
-			selectForward: true, // true 일 경우에 전에 선택한 날짜 이후의 날짜만 선택 가능하다
-			selectBackward: false,
-			showWeekNumbers: false,
-			getWeekNumber: function(date) //date will be the first day of a week
-			{
-				return moment(date).format('w');
-			},
-			monthSelect: true, // 월 드롭박스로 별도 선택 *********************
-			yearSelect: true // 년도 드롭박스로 별도 선택 *********************
-		}; 
-		
-//		picker = $.extend(picker,options)
-
-		$(id).dateRangePicker(picker);
-		
-	}
 	
 	
 	
 	
 	// ======================================================================
 	
-	ctrl.btnJqOpenHandler = function(e){
-		e.stopPropagation();
-		$('#divJQueryRange').data('dateRangePicker').open();
-	}
 	
 	return ctrl;
 }));
