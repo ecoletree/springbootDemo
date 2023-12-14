5e5b07842e4c3dcf623ceaf0f3572006985a6ea1
kkh <ecoletree@gmail.com>
2023-12-14 16:54:51 +0900

    암복호화 수정

--- bootWorkout-1/src/main/java/com/bootWorkout/demo1/kisaEncript/web/KISASeedCryptoRestController.java
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/kisaEncript/web/KISASeedCryptoRestController.java
@@ -33,8 +33,8 @@ public class KISASeedCryptoRestController {
 	public @ResponseBody Map<String,Object> cryptoTest(@RequestBody Map<String,Object> param){
 		
 		CryptoType typeSet = CryptoType.builder()
+				.is_kisa(param.get("is_kisa").toString()) //SEED/AES
 				.aes_type(param.get("aes_type").toString()) //CBC/ECB
-				.is_kisa(param.get("is_kisa").toString()) //Y/N
 				.build();
 		String result = CryptoCommon.setCryptoType(typeSet).decryptString(param.get("origin").toString());
 		
