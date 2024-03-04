7762a926090241423c902890d156acaf1f7355f0
kkh <ecoletree@gmail.com>
2024-03-04 17:35:35 +0900

    sse통신 테스트

---
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/sse/service/NotificationService.java
@@ -0,0 +1,27 @@
+/*****************************************************************
+ * Copyright (c) 2017 EcoleTree. All Rights Reserved.
+ *
+ * Author : Kim Kyung Hyun
+ * Create Date : 2024. 3. 4.
+ * File Name : NotificationService.java
+ * DESC :
+*****************************************************************/
+package com.bootWorkout.demo1.sse.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface NotificationService {
+
+	/**
+	 * @param id
+	 * @return
+	 */
+	public SseEmitter subscribe(Long id);
+
+	/**
+	 * @param id
+	 * @param string
+	 */
+	public void notify(Long id, Object event);
+
+}
---
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/sse/web/NotificationController.java
@@ -0,0 +1,50 @@
+/*****************************************************************
+ * Copyright (c) 2017 EcoleTree. All Rights Reserved.
+ *
+ * Author : Kim Kyung Hyun
+ * Create Date : 2024. 3. 4.
+ * File Name : NotificationController.java
+ * DESC :
+*****************************************************************/
+package com.bootWorkout.demo1.sse.web;
+
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.bootWorkout.demo1.sse.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+	private final NotificationService notificationService;
+
+    /** 클라이언트에서 구독하기 위함
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@PathVariable Long id) {
+        return notificationService.subscribe(id);
+    }
+
+    /** 클라이언트로 알림을 주기 위함
+     * @param id
+     */
+    @PostMapping("/send-data/{id}")
+    public void sendData(@PathVariable Long id, @RequestBody Map<String,Object> param){
+        notificationService.notify(id, param);
+    }
+
+}
