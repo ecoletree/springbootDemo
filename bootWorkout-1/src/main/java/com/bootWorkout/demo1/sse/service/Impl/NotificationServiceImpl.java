2b1161112b78e740749e85da7d8d63cb369dcc7a
kkh <ecoletree@gmail.com>
2024-03-27 14:59:57 +0900

    sse exception 유발

--- bootWorkout-1/src/main/java/com/bootWorkout/demo1/sse/service/Impl/NotificationServiceImpl.java
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/sse/service/Impl/NotificationServiceImpl.java
@@ -30,10 +30,22 @@ public class NotificationServiceImpl implements NotificationService {
 
 	@Override
 	public void notify(String id,Object event) {
-		notificationCommon.notify(id,event);
+		while(true) { //tread sleeping 걸어서 하거나 해서 에러 시간 늦추기
+			try{
+				String str = null;
+				str.indexOf(0);
+			}catch(Exception e) {
+				/*
+				 * id,message,stacktrace(원래 보내면 안되는데 개발자 보라
+				 * event -> error stacktrace 넣어서 보내게 됨
+				 * exception은 데이터 체크할때 많이 뜨는거고
+				 * onerror는 서버가 죽거나 해서 손댈수 없는 상황일때 보내짐
+				 */
+				notificationCommon.notify(id,event);
+			}
+		}
 
 	}
-
 
 	@Override
 	public void remove(String id) {
