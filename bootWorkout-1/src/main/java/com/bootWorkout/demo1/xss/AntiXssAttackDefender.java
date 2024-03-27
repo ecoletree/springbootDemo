fc2c5046ebd7ca02c7ea521da73994e9a179aab7
kkh <ecoletree@gmail.com>
2024-03-27 17:05:32 +0900

    AntiXSS 추가

---
+++ bootWorkout-1/src/main/java/com/bootWorkout/demo1/xss/AntiXssAttackDefender.java
@@ -0,0 +1,56 @@
+/*****************************************************************
+ * Copyright (c) 2017 EcoleTree. All Rights Reserved.
+ *
+ * Author : Kim Kyung Hyun
+ * Create Date : 2024. 3. 27.
+ * File Name : AntiXssAttackDefender.java
+ * DESC :
+*****************************************************************/
+package com.bootWorkout.demo1.xss;
+
+import org.owasp.encoder.Encode;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
+
+public class AntiXssAttackDefender {
+
+	public static void main(String[] args) {
+
+		// 사용자 입력값
+        String userInput = "asdfasfd";
+//        userInput = "<script>alert('XSS 공격 예제');</script>";
+//        userInput = "<img src='#' onerror='alert('xss공격!')'>";
+//        userInput = "<iframe src='106 097 118 097 115 099 114 105 112 116 058 097 108 101 114 116 040 039 088 083 083 032 234 179 181 234 178 169 032 236 152 136 236 160 156 039 041 059;'>";
+
+        // AntiXSS 라이브러리를 사용하여 XSS 공격 방어 : 안전한 값으로 변환
+        String sanitizeddText = AntiXSShtmlSanitizer(userInput);
+        String encodedText = AntiXSSEncoder(userInput);
+
+        // 안전한 값 출력
+        System.out.println("AntiXSShtmlSanitizer : " + sanitizeddText);
+        System.out.println("AntiXSSEncoder : " + encodedText);
+
+	}
+
+	/** convert
+	 * @param userInput
+	 * @return
+	 */
+	private static String AntiXSSEncoder(String inputText) {
+		String safeTxt = Encode.forHtml(inputText);
+		return safeTxt;
+	}
+
+	/** remove
+	 * @param inputText
+	 * @return
+	 */
+	private static String AntiXSShtmlSanitizer(String inputText) {
+		PolicyFactory policy = new HtmlPolicyBuilder().toFactory();
+		String txtResult = policy.sanitize(inputText);
+		return txtResult;
+	}
+
+
+
+}
